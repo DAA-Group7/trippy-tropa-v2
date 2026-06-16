9d2bfa2c1d229bfe810e421b34877aaa3cfac245
pogi <aaroncallanga01@gmail.com>
2026-06-16 22:36:14 +0800

    feat: landing page

---
+++ scratch/build_landing.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+
+const html = `<!DOCTYPE html>
+
+<html class="scroll-smooth" lang="en"><head>
+... (I will paste the full HTML here, but wait, the HTML is in the prompt).
+`;
